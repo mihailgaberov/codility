742150c2530a5e5cac0c4247b7fd497c18736f4d
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-05-18 15:48:44 +0200

    Added Equi task.

---
+++ equi/README.md
@@ -0,0 +1,16 @@
+# Equi
+
+Equi is a task to create a function which accepts an array of integers and return any of its equilibrium indices or -1.
+An equilibrium index of this array is any integer P such that 0 ≤ P < N and the sum of elements of lower indices is equal to the sum of elements of higher indices, i.e. 
+
+## Install
+
+```
+Install NodeJS (https://nodejs.org/en/)
+```
+
+## Run
+
+```
+node equi.js (via cmd in the same dir as the file)
+```
---
+++ equi/equi.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+rl.question('Input: ', (arr) => {
+    console.log('');
+    console.log('Output: ', getEqui([-1, 3, -4, 5, 1, -6, 2, 1]));
+    console.log('');
+    rl.close();
+});
+
+function solution (arr) {
+    if (!arr)
+        return -1;
+    
+    var sum = 0;
+
+    var i;
+    var len = arr.length;
+
+    for (i = 0; i < len; i++) {
+        sum += arr[i];
+    }
+
+    var sumLeft = 0;
+
+    for (i = 0; i < n; i++) {
+        var sumRight = sum - sumLeft - arr[i];
+        if (sumLeft === sumRight)
+            return i;
+
+        sumLeft += arr[i];
+
+    }
+
+    return -1;
+}
